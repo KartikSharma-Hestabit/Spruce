18326091d06a4de46f07ab363e2e20f2e0788229
Kartik kumar <kartik2.hestabit@gmail.com>
2024-05-01 11:59:29 +0530

    Spruce 2nd commit - 1/05/2024 11:44 AM

--- app/src/main/java/com/example/spruce/di/RetrofitModule.kt
+++ app/src/main/java/com/example/spruce/di/RetrofitModule.kt
@@ -13,7 +13,7 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object RetrofitModule {
-
+//
     @Provides
     fun provideBaseUrl(): String = "https://spruce.hestawork.com/"
 
